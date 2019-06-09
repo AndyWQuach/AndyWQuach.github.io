11d51151486f75a9be26755d6e7afc1c5a5c3573
Andy <me@andyquach.com>
2019-06-09 13:21:25 +0100

    Added Python files

--- websiteapp/app.py
+++ websiteapp/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template('home.html')
-
-@app.route("/about/")
-def about():
-	return render_template('about.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+app.config.from_pyfile('settings.py')
+pages = FlatPages(app)
+freezer = Freezer(app)
